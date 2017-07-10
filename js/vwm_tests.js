b9dd6c3ee6b3b21510c3b7a7b158ecf4c9179c3f
Ryan Achten <ryanachten@gmail.com>
2017-07-10 16:01:50 +1200

    Add progress circle functionality denoting current test index

--- js/vwm_tests.js
+++ js/vwm_tests.js
@@ -1,20 +1,40 @@
 // DESCRIPT: all psych test controller functionality goes here:
 
-var testCount = 10;
-var curTest = 0;
-var testProgressIcons;
-
-console.log('lissajousVariants.length: ' + lissajousVariants.length);
+var totalTestCount = 10;
+var curTestIndex = null;
 
 var curLassijIndex;
+var testProgressIcons;
+
+
 function loadTest(){
+
+	if(curTestIndex === null){
+		curTestIndex = 0;
+	}else{
+		curTestIndex++;
+	}
+
+	if(curTestIndex+1 <= totalTestCount){
+		console.log('curTestIndex: ' + curTestIndex);
+		updateLissajFigure();
+		updateProgressIcons();
+	}	
+}
+
+function updateProgressIcons(){
+		
+	testProgressIcons[curTestIndex].children[0].style.fill = '#FFA0B6';
+}
+
+function updateLissajFigure(){
 	var newLassijIndex = Math.floor(Math.random()*lissajousVariants.length);
 	console.log('curLassij: ' + curLassijIndex + ' newLassij: ' + newLassijIndex);
 	if(newLassijIndex !== curLassijIndex){
 		tweenLissaj(lissajousVariants[ newLassijIndex ]);
-		curLassijIndex = newLassijIndex;
+		curLassijIndex = newLassijIndex;		
 	}
 	else{
-		loadTest();
+		updateLissajFigure();
 	}
 }
