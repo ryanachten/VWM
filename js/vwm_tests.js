0b280b3170d511f80f8a9476e2cfcb84c37a4cb9
Ryan Achten <ryanachten@gmail.com>
2017-07-13 12:41:06 +1200

    Log option button id in event listener, Chain transitions between N-Back stages

--- js/vwm_tests.js
+++ js/vwm_tests.js
@@ -4,7 +4,7 @@ var testPerStageCount = 25;
 var curSceneIndex = 0;
 var curTestIndex = null;
 
-var nback = 4;
+var nback = 0;
 
 var curLassijIndex;
 
@@ -15,28 +15,34 @@ var targetHelperText = document.getElementById('target-helpertext');
 var optionsHelperText = document.getElementById('options-helpertext');
 
 
-function loadScene(){
-
+function loadScene(optionButtonID){
+	console.log(' ');
 	console.log('curSceneIndex: '+ curSceneIndex);
 
 	if(curSceneIndex < nback){ //if the number of mem tests req hasn't been reached
 		console.log('sceneMode: memoriseOnly');
 		loadMemoriseOnlyScene();
+		curSceneIndex++;
 	}
 	else if(curTestIndex+nback >= testPerStageCount //if we're on the last mem scene (accounts for n-back range)
 			&& curTestIndex < testPerStageCount){  	// and not over test count (25)
 		console.log('sceneMode: testOnly');
-		loadTestOnlyScene();
+		loadTestOnlyScene(optionButtonID);
+		curSceneIndex++;
 	} 
 	else if (curTestIndex < testPerStageCount){
 		console.log('sceneMode: testAndMemorise');
-		loadTestMemoriseScene(); 
+		loadTestMemoriseScene(optionButtonID);
+		curSceneIndex++; 
 	}
-	curSceneIndex++;
+	else if(curSceneIndex+nback > testPerStageCount){
+		console.log('sceneMode: transition to next stage');
+		loadStageTransitionScene(optionButtonID);
+	}
 }
 
 
-function loadTestMemoriseScene(){
+function loadTestMemoriseScene(optionButtonID){
 
 	function initScene(){
 		var continueButton = document.getElementById('continue-button');
@@ -52,16 +58,20 @@ function loadTestMemoriseScene(){
 		curTestIndex = 0;
 	}
 	else{
+		console.log('curTestIndex: ' + curTestIndex);
+		if(optionButtonID !== 'continue-button'){
+			console.log('optionButtonID: ' + optionButtonID);
+		}
 		curTestIndex++;
 	}
 
-	if(curTestIndex+1 <= testPerStageCount){
-		console.log('curTestIndex: ' + curTestIndex);
+	// if(curTestIndex+1 <= testPerStageCount){ //needed - already tested for in loadScene?
+		
 		updateLissajFigure();
 		updateProgressBar();
 		updateTrialCountText();
 		updateHelperText('testMem');
-	}
+	// }
 }
 
 function loadMemoriseOnlyScene(){
@@ -80,18 +90,46 @@ function loadMemoriseOnlyScene(){
 	updateLissajFigure();
 }
 
-function loadTestOnlyScene(){
+function loadTestOnlyScene(optionButtonID){
 
 	function initScene(){
 		lissajousCurve.color = '#CFECEC'; //FIXME: seems kind of buggy - not best approach
+		updateLissajFigure();
 	}
 	initScene();
 
+	console.log('curTestIndex: ' + curTestIndex);
+	console.log('optionButtonID: ' + optionButtonID);
 	curTestIndex++;
 
 	updateProgressBar();
 	updateTrialCountText();
 	updateHelperText('test');
+}
+
+function loadStageTransitionScene(optionButtonID){
+	function initScene(){
+		lissajousCurve.color = '#CFECEC';
+		updateLissajFigure();
+		var continueButton = document.getElementById('continue-button');
+				continueButton.style.display = 'inline-block';
+		var optionButtons = document.getElementsByClassName('option-button');
+			for (var i = 0; i < optionButtons.length; i++) {
+				optionButtons[i].style.display = 'none';
+			}
+	}
+	initScene();
+
+	console.log('curTestIndex: ' + curTestIndex);
+	if(optionButtonID !== 'continue-button'){
+		console.log('optionButtonID: ' + optionButtonID);
+	}
+
+	curSceneIndex = 0;
+	curTestIndex = null;
+	nback++;
+	console.log(' ');
+	console.log('Move to N-back: ' + nback);
 }
 
 
