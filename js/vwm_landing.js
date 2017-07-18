7433af1c52dea3ddc86f5a5853834b5c4f19b73a
Ryan Achten <ryanachten@gmail.com>
2017-07-19 10:31:17 +1200

    Update lissag references to account for group sets

--- js/vwm_landing.js
+++ js/vwm_landing.js
@@ -1,4 +1,4 @@
-// DESCRIPT: all psych test controller functionality goes here:
+// DESCRIPT: all landing page functionality goes here:
 
 var screenWidth, screenHeight,
 	scene, camera,
@@ -9,15 +9,15 @@ var lissajousCurve;
 
 $('html').hide(); //FOUC hack
 $(document).ready(function(){
+	initData()
 	initScene();
 	animate();
-	var newLassijIndex = Math.floor(Math.random()*lissajousVariants.length);
-	tweenLissaj(lissajousVariants[ newLassijIndex ]);
+	var newLassijGroup = Math.floor(Math.random()*lissajousGroups.length);
+	var newLassijIndex = Math.floor(Math.random()*lissajousGroups[newLassijGroup].length);
+	tweenLissaj(lissajousGroups[newLassijGroup][newLassijIndex]);
 
 	$('html').show(); //FOUC hack
 });
-
-var clock;
 
 
 function animate() {
@@ -31,7 +31,6 @@ function render(){
 	renderer.render(scene, camera);
 }
 
-
 function initScene() {
 
 	calcCanvasSizes();
@@ -40,8 +39,48 @@ function initScene() {
 	initCamera();
 	initLight();
 	initMesh();
-}
 
+	function initSceneRenderer(){	
+		scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0xCFECEC );
+		if ( Detector.webgl )
+			renderer = new THREE.WebGLRenderer( {antialias:true} );
+		else
+			console.log('WebGLRenderer not supported - please use another browser');
+			//TODO - add proper feedback
+		renderer.setSize(screenWidth, screenHeight);
+		document.getElementById('view-container').appendChild(renderer.domElement);
+	}
+
+
+	function initCamera(){
+		camera = new THREE.PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 1000);
+		camera.position.set(0,150,180);
+		camera.lookAt(scene.position);
+		scene.add(camera);
+	}
+
+
+	function initLight(){
+		light = new THREE.PointLight(0xffffff);
+		light.position.set(100,250,0);
+		scene.add(light);
+
+		ambientLight = new THREE.AmbientLight(0x444444);
+		
+		scene.add(ambientLight);
+	}
+
+
+	function initMesh(){
+		lissajousCurve = new LissajousCurve();
+		lissajousCurve.color = "#BFE3E3";
+		lissajousCurve.sizeX = lissajousCurve.sizeY = lissajousCurve.sizeZ = 150;
+		lissajousCurve.meshObject.name = "Lissa";
+		lissajousCurve.createMesh();
+		scene.add(lissajousCurve.meshObject);
+	}
+}
 
 function calcCanvasSizes(){
 	
@@ -52,7 +91,6 @@ function calcCanvasSizes(){
 	screenWidth = $(window).width();
 	console.log('screenWidth: ' + screenWidth);
 }
-
 
 window.addEventListener('resize', recalcCanvas, false);
 function recalcCanvas(){
@@ -65,52 +103,18 @@ function recalcCanvas(){
 }
 
 
-function initSceneRenderer(){	
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xCFECEC );
-	if ( Detector.webgl )
-		renderer = new THREE.WebGLRenderer( {antialias:true} );
-	else
-		console.log('WebGLRenderer not supported - please use another browser');
-		//TODO - add proper feedback
-	renderer.setSize(screenWidth, screenHeight);
-	document.getElementById('view-container').appendChild(renderer.domElement);
+$('#email-inputform').submit(function(e) {
+	e.preventDefault();
+	nameEmailFormValidationRedirect();
+});
+
+function nameEmailFormValidationRedirect(){
+
+	if(document.getElementById('name-inputfield').value.length === 0){
+		alert('Please enter your name before continuing');
+	}else{
+		//TODO: store username in DB
+		console.log('submitting');
+		window.location.href = 'test.html';	
+	}	
 }
-
-
-function initCamera(){
-	camera = new THREE.PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 1000);
-	camera.position.set(0,150,180);
-	camera.lookAt(scene.position);
-	scene.add(camera);
-}
-
-
-function initLight(){
-	light = new THREE.PointLight(0xffffff);
-	light.position.set(100,250,0);
-	scene.add(light);
-
-	ambientLight = new THREE.AmbientLight(0x444444);
-	
-	scene.add(ambientLight);
-}
-
-
-function initMesh(){
-	lissajousCurve = new LissajousCurve();
-	lissajousCurve.color = "#BFE3E3";
-	lissajousCurve.sizeX = lissajousCurve.sizeY = lissajousCurve.sizeZ = 150;
-	lissajousCurve.meshObject.name = "Lissa";
-	lissajousCurve.createMesh();
-	scene.add(lissajousCurve.meshObject);
-}
-
-
-$('#name-inputform').submit(function(e) {
-    e.preventDefault();
-
-	//TODO: store username in DB
-	console.log('submitting');
-	window.location.href = 'test.html';
-});	
